91795205786a0c2924eb1436264be3c1d1128406
yjq <102948996@qq.com>
2018-08-12 23:00:38 +0800

    前端代码封装以及 主界面添加模块

--- src/main/resources/static/js/lib/common.js
+++ src/main/resources/static/js/lib/common.js
@@ -4,11 +4,11 @@
 define(function () {
     
     var common = {};
+    common.body = $("body");
     
     common.warnning = function (content) {
         var warnningDialog = "";
         var noWarnningExist = $("#warnningDialog").length == 0;
-        var body = $("body");
         if (noWarnningExist){
             warnningDialog = $("<div id='warnningDialog' class='warnningDialog'></div>");
         }else{
@@ -23,9 +23,50 @@ define(function () {
         warnningDialog.append(alertDialog);
 
         if (noWarnningExist){
-            body.append(warnningDialog);
+            this.body.append(warnningDialog);
         }
         warnningDialog.fadeIn(100).fadeOut(3000);
+    }
+    
+    common.ajax = function (obj,middleware) {
+        var deffered = $.Deferred();
+        var _this = this;
+        _this.ajaxLoading();
+        $.ajax({
+            url: obj.url,
+            type: obj.type || "GET",
+            data: obj.data || {},
+            dataType: obj.dataType || "json",
+            success:function (data) {
+                if (data.code == 200){
+                    deffered.reject(data);
+                }else{
+                    deffered.resolve(data);
+                }
+                _this.ajaxLoadingComplete();
+            },
+            error:function () {
+                deffered.reject("接口出错，请重试或者联系管理员");
+            }
+        });
+
+        if(!middleware){
+            middleware = function () {}
+        }
+        return deffered.done(middleware).fail(function () {})
+    }
+
+    common.ajaxLoading = function () {
+        if($("#ajaxLoading").length == 0){
+            var loading = $("<div id='ajaxLoading'></div>");
+            var loading_pic = $("<span class='fa fa-spinner loading-circle'></span>");
+            loading.append(loading_pic);
+            this.body.append(loading);
+        }
+        $("#ajaxLoading").show();
+    }
+    common.ajaxLoadingComplete = function () {
+        $("#ajaxLoading").hide();
     }
 
     return common;
